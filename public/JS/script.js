e225a9baa803fd0c04fe628241d50f09f91e913e
isomdurm <isomdurm@gmail.com>
2015-10-25 23:41:30 -0700

    Fully Functional Simon Says Easter Egg

--- public/JS/script.js
+++ public/JS/script.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function(){
 
 	$('#logo').addClass('grow').addClass('photo-logo').addClass('background-size-logo');
@@ -20,10 +21,9 @@ $(document).ready(function(){
 
 	$('#form-login').addClass('hideForm');
 
-	$('.simon-says-circle').addClass('hideForm');
+	$('.wrapper').addClass('hideForm');
 
 	var clicks = 0;
-
 
 	$('#welcome').on('click', function() {
 		if ($('#logo').hasClass('photo-logo') && clicks == 1) {
@@ -38,11 +38,11 @@ $(document).ready(function(){
 			$('#logo').addClass('shrink');
 			$('#logo').one('webkitAnimationEnd', function() {
 				$('#logo').removeClass().addClass('hideForm');
-				$('.simon-says-circle').removeClass('hideForm').addClass('grow');
+				$('.wrapper').removeClass('hideForm').addClass('grow');
+				Simon.init();
 			});
 		};
 	});
-
 
 	$('.one').on('click', function() {
 		if ($('.two').hasClass('photo-logo')) {
@@ -153,45 +153,6 @@ $(document).ready(function(){
 		};
 	});
 
-
-
-
-
-	// $('.two').one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
-	// 	$('.two').removeClass('fadeOut').addClass('image-logo').addClass('fadeIn');
-	// 	// $('#begin').addClass('image-new-user').addClass('fadeIn').addClass('background-white');
-	// });
-
-
-	// $('#signUpButton').on('click', function(e) {
-	// 	e.preventDefault();
-	// 	$('#begin').removeClass('grow').addClass('shrink');
-	// 	$('#welcome').removeClass('fadeIn').addClass('fadeOut');
-	// 	$('#begin').one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
-	// 		$('#begin').addClass('hideForm');
-	// 		$('.uploadPhoto').append("<p class='uploader-new-button center-block' id='uploaderPhoto'><input type='hidden' name='picture' role='uploadcare-uploader'></p>");
-	// 		$('#uploaderPhoto').addClass('hideForm');
-	// 		$('#userPhoto').removeClass('hideForm').addClass('grow');
-	// 		$('#new-profile-form').removeClass('hideForm').addClass('fadeIn');
-	// 	});	
-	// 	$('#welcome').one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
-	// 		$('#welcome').removeClass('fadeIn').addClass('hideForm');
-	// 	});
-	// 	$('#userPhoto').one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
-	// 		$('#uploaderPhoto').removeClass('hideForm').addClass('fadeIn');
-	// 	});
-	// });
-
-
-
-
-
-
-
-
-
-
-
 	$('#new-profile-form').on('submit', function(e) {
 		e.preventDefault();
 		var newUser = $(this).serialize();
@@ -205,8 +166,144 @@ $(document).ready(function(){
 		
 	});
 
+
+	var Simon = {
+		sequence: [],
+		copy: [],
+		round: 0,
+		active: true,
+		mode: 'normal',
+	
+		init: function() {
+			var that = this;
+			$('[data-action=start]').on('click', function() {
+				that.startGame();
+			});
+			$('input[name=mode]').on('change', function(e) {
+				that.changeMode(e);
+			});
+		},
+
+		startGame: function() {
+			this.sequence = [];
+			this.copy = [];
+			this.round = 0;
+			this.active = true;
+			$('p[data-action="lose"]').hide();
+			this.newRound();
+		},
+
+		// add a new color to the sequence and animate it to the user
+		newRound: function() {
+			$('[data-round]').text(++this.round);
+			this.sequence.push(this.randomNumber());
+			this.copy = this.sequence.slice(0);
+			this.animate(this.sequence);
+		},
+
+		// the game is controlled primarily through this function, along with checkLose().
+		// Since the player can never actually "win", we just listen for clicks as the user 
+		// plays the sequence and each time, check if they lost
+		registerClick: function(e) {
+			var desiredResponse = this.copy.shift();
+			var actualResponse = $(e.target).data('tile');
+			this.active = (desiredResponse === actualResponse);
+			this.checkLose();
+		},
+
+		// three possible situations:
+		// 1. The user clicked the wrong color (end the game)
+		// 2. The user entered the right color, but is not finished with the sequence (do nothing)
+		// 3. The user entered the right color and just completed the sequence (start a new round)
+		checkLose: function() {
+			// copy array will be empty when user has successfully completed sequence
+			if (this.copy.length === 0 && this.active) {
+				this.deactivateSimonBoard();
+				this.newRound();
+
+			} else if (!this.active) { // user lost
+				this.deactivateSimonBoard();
+				this.endGame();
+			}
+		},
+
+		endGame: function() {
+			// notify the user that they lost and change the "round" text to zero
+			$('p[data-action=lose]').show();
+			$($('[data-round]').get(0)).text('0');
+		},
+
+		changeMode: function(e) {
+			this.mode = e.target.value;
+		},
+
+		/*----------------- Helper functions -------------------*/
+
+		// allow user to interact with the game
+		activateSimonBoard: function() {
+			var that = this;
+			$('.simon')
+				.on('click', '[data-tile]', function(e) {
+					that.registerClick(e);
+				})
+
+				.on('mousedown', '[data-tile]', function(){
+					$(this).addClass('active');
+				})
+
+				.on('mouseup', '[data-tile]', function(){
+					$(this).removeClass('active');
+				});
+
+			$('[data-tile]').addClass('hoverable');
+		},
+
+		// prevent user from interacting until sequence is done animating
+		deactivateSimonBoard: function() {
+			if (this.mode !== 'free-board') {
+				$('.simon')
+					.off('click', '[data-tile]')
+					.off('mousedown', '[data-tile]')
+					.off('mouseup', '[data-tile]');
+
+				$('[data-tile]').removeClass('hoverable');
+			}
+		},
+
+		animate: function(sequence) {
+			var i = 0;
+			var that = this;
+			var interval = setInterval(function() {
+				that.lightUp(sequence[i]);
+
+				i++;
+				if (i >= sequence.length) {
+					clearInterval(interval);
+					that.activateSimonBoard();
+				}
+			}, 600);
+		},
+
+		lightUp: function(tile) {
+			if (this.mode !== 'sound-only') {
+				var $tile = $('[data-tile=' + tile + ']').addClass('lit');
+				window.setTimeout(function() {
+					$tile.removeClass('lit');
+				}, 300);
+			}
+
+		},
+
+		randomNumber: function() {
+			// between 1 and 4
+			return Math.floor((Math.random()*4)+1);
+		}
+	};
+
+	return Simon;
 });
 
-// red, pink, yellow, blue, purple, green, orange, white
+
+
 
 
